fe853a4c37039883c4d4e3c1d6910d357d29262f
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2022-11-12 00:18:45 +0000

    feat: Control game flow

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,11 +1,14 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
-fn main() {
-    println!("Guess the number!");
+/**
+ * Run guessing game
+ * @params {u32} secret_number - random generated number
+ * @params {i32} tries - number of tries
+ */
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut tries = 3;
+fn run_guess_game(secret_number: u32, mut tries: i32) {
+    println!("Guess the number!");
 
     loop {
         println!("Tries Left: {}\nPlease input your guess.", tries);
@@ -21,7 +24,7 @@ fn main() {
             Err(_) => {
                 println!("Input must be a number!!!");
                 continue;
-            },
+            }
         };
 
         println!("You guessed: {guess}");
@@ -31,22 +34,59 @@ fn main() {
                 println!("Too Small!");
                 tries = tries - 1;
                 if tries == 0 {
-                    println!("You are out of tries!");
+                    println!("You are out of tries!\nPress C to continue playing or Q to Quit game.");
                     break;
                 }
-            },
+            }
             Ordering::Equal => {
                 println!("You Win! {} is the correct guess.", guess);
+                println!("Press C to continue playing or Q to Quit game.");
                 break;
             }
             Ordering::Greater => {
                 println!("Too Big!");
                 tries = tries - 1;
                 if tries == 0 {
-                    println!("You are out of tries!");
+                    println!("You are out of tries!\nPress C to continue playing or Q to Quit game.");
                     break;
                 }
-            },
+            }
         }
     }
+}
+
+/**
+ * This is the main program to run.
+ */
+fn main() {
+    println!("Welcome to Guess...");
+    println!("Press N to start New Game or Q to Quit game.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let tries = 3;
+
+    loop {
+        let mut command = String::new();
+
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        match &command.trim().to_lowercase() as &str {
+            "n" => {
+                // start new guessing game
+                run_guess_game(secret_number, tries)
+            },
+            "c" => {
+                // continue playing guessing game
+                run_guess_game(secret_number, tries)
+            },
+            "q" => {
+                // quit guessing game
+                println!("Quitting game...");
+                break;
+            },
+            _ => println!("Invalid command!\nPress N to start New Game or Q to Quit game.")
+        }
+    }    
 }
